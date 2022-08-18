5a210d32a32c00f1e0a197a0167dc386488e8f17
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-08-18 16:25:41 -0300

    feat(Projects): Adcionando novos projetos

--- src/components/Projetos.js
+++ src/components/Projetos.js
@@ -11,6 +11,10 @@ import Typography from '@material-ui/core/Typography';
 
 import shoppingCart from '../images/shopping-cart.png';
 import frontEndOnlineSore from '../images/front-end-online-store.png'
+import recipesApp from '../images/recipes01.png'
+import tfc from '../images/tfc.gif'
+import ebytr from '../images/ebytr.png'
+import blogsApi from '../images/blogs-api.png'
 
 const useStyles = makeStyles((theme) => ({
   mainContainer: {
@@ -25,7 +29,13 @@ const useStyles = makeStyles((theme) => ({
 
 const projects = [
   {
-    name: "Shopping Cart",
+    name: "Full-Stack Ebytr",
+    description: `Projeto referente a Blitz de carreira da Trybe, onde fomos provocados a fazer uma aplicação Full-Stack para Criação, Edição e Deleção de Tarefas. O objetivo foi disponibilizar um front-end para o colaborador se cadastrar e poder fazer controle das tarefas.`,
+    image: ebytr,
+    url: 'https://recipes-re-appl.vercel.app/',
+  },
+  {
+    name: "Front-End Shopping Cart",
     description: `Projeto de conclusão do curso de Fundamentos do curso da Trybe, onde fazemos um loja utilizando API do mercado livre`,
     image: shoppingCart,
     url: 'https://rafaelsantosmg.github.io/project-shopping-cart/',
@@ -37,11 +47,23 @@ const projects = [
     url: 'https://front-end-online-store-omega.vercel.app/',
   },
   {
-    name: "APP de Receitas",
+    name: "Front-End APP de Receitas",
     description: `Projeto de conclusão de Front-end. Desenvolvido utilizando React, Context API, Hooks e metodologias ágeis`,
-    image: frontEndOnlineSore,
+    image: recipesApp,
     url: 'https://recipes-re-appl.vercel.app/',
-  }
+  },
+  {
+    name: "Back-End Blogs API",
+    description: `Projeto desenvolvido utilizando NodeJs, Express, Sequelize, JWT`,
+    image: blogsApi,
+    url: 'https://github.com/rafaelsantosmg/backend-blogs-api',
+  },
+  {
+    name: "Back-End TFC",
+    description: `Projeto de integração onde fomos provocados a criar todo "back-end" utilizando a biblioteca "express" com “TypeScript”, e colocando em prática os conhecimentos em POO e SOLID.`,
+    image: tfc,
+    url: 'https://github.com/rafaelsantosmg/backend-poo-trybe-futebol-clube',
+  },
 ];
 
 const Projetos = () => {
